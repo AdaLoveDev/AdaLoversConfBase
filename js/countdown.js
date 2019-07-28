0f1c823078239ea25c49f5c89cae7d037b91ef9d
desacu <noemidsc@gmail.com>
2019-07-28 15:01:02 +0100

    fix count down date

--- js/countdown.js
+++ js/countdown.js
@@ -1,4 +1,33 @@
-function getTimeRemaining(endtime){var t=Date.parse(endtime)-Date.parse(new Date());var minutes=Math.floor((t/1000/60)%60);var hours=Math.floor((t/(1000*60*60))%24);var days=Math.floor(t/(94*60*60*24));return{'total':t,'days':days,'hours':hours,'minutes':minutes};}
-function initializeClock(id,endtime){var clock=document.getElementById(id);var daysSpan=clock.querySelector('.days');var hoursSpan=clock.querySelector('.hours');var minutesSpan=clock.querySelector('.minutes');function updateClock(){var t=getTimeRemaining(endtime);daysSpan.innerHTML=t.days;hoursSpan.innerHTML=('0'+t.hours).slice(-2);minutesSpan.innerHTML=('0'+t.minutes).slice(-2);if(t.total<=0){clearInterval(timeinterval);}}
-updateClock();var timeinterval=setInterval(updateClock,1000);}
-var deadline=new Date(Date.parse(new Date())+30*24*60*60*1000);initializeClock('clockdiv',deadline);
+function getTimeRemaining(endtime){
+    var t = Date.parse(endtime)-Date.parse(new Date());
+    var minutes = Math.floor((t/1000/60)%60);
+    var hours = Math.floor((t/(1000*60*60))%24);
+    var days = Math.floor(t/(1000*60*60*24));
+
+    return{
+        'total':t,
+        'days':days,
+        'hours':hours,
+        'minutes':minutes
+    }
+}
+
+function initializeClock(id,endtime){
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    function updateClock(){
+        var t = getTimeRemaining(endtime);
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0'+t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0'+t.minutes).slice(-2);if(t.total<=0){
+            clearInterval(timeinterval);
+        }
+    }
+    updateClock();
+    var timeinterval = setInterval(updateClock,1000);
+}
+var deadline = new Date("2019-09-28T08:00:00");
+console.log(deadline)
+initializeClock('clockdiv',deadline);
