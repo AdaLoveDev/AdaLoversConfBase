e555c463373cac69465f55dc4b1f9e078138ec17
Alelit4 <alelit4@gmail.com>
2019-08-04 19:33:14 +0100

    navbar jump solved

--- js/main.js
+++ js/main.js
@@ -2,20 +2,6 @@ $(function() {
     "use strict";
     var nav_offset_top = $('header').height() + 50;
 
-    function navbarFixed() {
-        if ($('.header_area').length) {
-            $(window).scroll(function() {
-                var scroll = $(window).scrollTop();
-                if (scroll >= nav_offset_top) {
-                    $(".header_area").addClass("navbar_fixed");
-                } else {
-                    $(".header_area").removeClass("navbar_fixed");
-                }
-            });
-        };
-    };
-    navbarFixed();
-
     $('li a').click(function(e) {
         //e.preventDefault();
         var $this = $(this);
